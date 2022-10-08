146ec54bb730801df27de8d476a77197e5b1f763
Kledson Basso <kledsonhugo@gmail.com>
2022-10-08 10:52:14 -0300

    Config Terraform Provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,23 @@
 # PROVIDER
 terraform {
 
+  required_version = "~> 1.3.1"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.34"
     }
   }
 
   backend "s3" {
     bucket         = "tf-state-notifier-bucket"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
     dynamodb_table = "tf-state-notifier-table"
   }
 
+}
+
+provider "aws" {
+  region  = "us-east-1"
 }
