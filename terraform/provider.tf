32543c512b88634a83b8ca3b31d24a5fe6ca8606
mateus tenorio <mateus.tenorio@hotmail.com>
2023-04-26 21:04:18 -0300

    IaC - Checkpoint 3 - Mateus Tenorio

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -23,5 +23,5 @@ provider "aws" {
   region                   = "us-east-1"
   shared_config_files      = ["./.aws/config"]
   shared_credentials_files = ["./.aws/credentials"]
-  profile                  = "fiap"
+  profile                  = "mateus"
 }
