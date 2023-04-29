d4873053834e091cc2f2ffd92c82e4d36ecc9476
Lurickardo <luizr726@gmail.com>
2023-04-29 01:25:52 -0300

    Finalizando checkpoint 3 padrao

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -21,7 +21,7 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = ["./.aws/config"]
-  shared_credentials_files = ["./.aws/credentials"]
-  profile                  = "fiap"
+  shared_config_files      = [".aws/config"]
+  shared_credentials_files = [".aws/credentials"]
+  profile                  = "luiz-fiap"
 }
