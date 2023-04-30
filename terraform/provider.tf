e3a9fb2608c5b2ea97602858bdcef488bddd3137
Yumi Tokuo <yumitokuo@gmail.com>
2023-04-30 14:08:10 -0300

    Checkpoint 3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -21,7 +21,7 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = ["./.aws/config"]
-  shared_credentials_files = ["./.aws/credentials"]
+  shared_config_files      = [".aws/config"]
+  shared_credentials_files = [".aws/credentials"]
   profile                  = "fiap"
 }
